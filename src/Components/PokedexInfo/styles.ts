54b2d3cea9185f1595a171f85121d502dbb7c84d
Mateus Faria <mateusfariadomingos@hotmail.com>
2022-05-28 20:12:41 -0400

    🐛 FIX: Padding inline

--- src/Components/PokedexInfo/styles.ts
+++ src/Components/PokedexInfo/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const TextBox = styled.p`
   font-size: 1rem;
   text-align: left;
-  padding-left: 1.5rem;
+  padding-inline: 1.5rem;
 
   &:first-letter {
     text-transform: uppercase;
