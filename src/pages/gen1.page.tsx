d4b3b19928a63e75ccf745f45a3e2bd5fc1972a4
Arthur Polon <arthurpolon@hotmail.com>
2022-11-26 14:47:06 -0300

    👌 IMPROVE: add 'load more' to fetch partially

--- src/pages/gen1.page.tsx
+++ src/pages/gen1.page.tsx
@@ -1,24 +1,53 @@
 import axios from "axios";
 import Head from "next/head";
-import { useState } from "react";
-import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
 import CardSection from "../Components/CardSection";
 import * as S from "./gen1.styles";
 import { TPokemonData } from "~types/TPokemon";
 import { BackToTopButton } from "src/Components/BackToTopButton";
 import { HaikeiBackground } from "./home.styles";
 
+const TOTAL_ITEMS = 151;
+const ITEMS_PER_PAGE = 10;
+
+const TOTAL_PAGES = Math.ceil(TOTAL_ITEMS / ITEMS_PER_PAGE);
+
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export default function Home() {
-  const { data: pokemonApiResponse } = useSWR<TPokemonData>(
-    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151",
-    fetcher
-  );
+  const getKey = (index: number) => {
+    if (index > TOTAL_PAGES) {
+      return null;
+    }
 
-  if (!pokemonApiResponse) {
+    let params = `offset=${index * ITEMS_PER_PAGE}&limit=${ITEMS_PER_PAGE}`;
+
+    const totalItemsShown = (index + 1) * ITEMS_PER_PAGE;
+
+    if (totalItemsShown > TOTAL_ITEMS) {
+      const itemsLeft = TOTAL_ITEMS - index * ITEMS_PER_PAGE;
+
+      params = `offset=${index * ITEMS_PER_PAGE}&limit=${itemsLeft}`;
+    }
+
+    return `https://pokeapi.co/api/v2/pokemon?${params}`;
+  };
+
+  const { data, size, setSize } = useSWRInfinite<TPokemonData>(getKey, fetcher);
+
+  const fetchNext = () => {
+    if (size >= TOTAL_PAGES) {
+      return;
+    }
+
+    setSize((state) => state + 1);
+  };
+
+  if (!data) {
     return null;
   }
+
+  const pkmList = data?.map((response) => response.results).flat();
 
   return (
     <S.Homepage>
@@ -27,7 +56,9 @@ export default function Home() {
       </Head>
       <BackToTopButton />
       <HaikeiBackground />
-      <CardSection pkmList={pokemonApiResponse.results} />
+      <CardSection pkmList={pkmList} />
+
+      <button onClick={fetchNext}>Load More</button>
     </S.Homepage>
   );
 }
